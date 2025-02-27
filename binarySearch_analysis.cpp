dcf9e4d3b7591d411fe9e255718606606e55720a
Ansh Kapoor <82038066+axdy02@users.noreply.github.com>
2025-02-27 09:46:25 +0530

    Update binarySearch_analysis.cpp

--- binarySearch_analysis.cpp
+++ binarySearch_analysis.cpp
@@ -1,58 +1,75 @@
-#include <iostream>
-#include <vector>
-#include <ctime>
-#include <cstdlib>
-using namespace std;
+import java.util.Random;
+import java.util.ArrayList;
 
-int stepCount=0;
-int recursiveBinarySearch(vector<int>& arr,int left,int right,int target){
-    stepCount++;
-    if(left<=right){
-        int mid=left+(right-left)/2;
-        if(arr[mid]==target)return mid;
-        else if(arr[mid]>target)return recursiveBinarySearch(arr,left,mid-1,target);
-        else return recursiveBinarySearch(arr,mid+1,right,target);
-    }
-    return -1;
-}
+public class BinarySearchAnalysis {
 
-void analyzbinarysearchRecursive(){
-    vector<int> inputSizes={10,20,30,40,50,100};
-    for(int i=0;i<inputSizes.size();i++){
-        int size=inputSizes[i];
-        vector<int> arr(size);
-        for(int i=0;i<size;i++){
-            arr[i]=i+1; 
+    // Global variable to count the number of steps
+    static int stepCount = 0;
+
+    // Recursive binary search function
+    static int recursiveBinarySearch(ArrayList<Integer> arr, int left, int right, int target) {
+        stepCount++;
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr.get(mid) == target) {
+                return mid;
+            } else if (arr.get(mid) > target) {
+                return recursiveBinarySearch(arr, left, mid - 1, target);
+            } else {
+                return recursiveBinarySearch(arr, mid + 1, right, target);
+            }
         }
-    // cout<<"Array: [ ";
-    // for(int i=0;i<size;i++){
-    //     cout<<arr[i]<<" ";
-    // }cout<<"]"<<endl;
+        return -1;
+    }
 
-    int bestcase=arr[(size-1)/2];
-    int worstcase=arr[size-1];
-    srand(time(0));
-    int randomindex=rand()%size;
-    int randomcase=arr[randomindex];
+    // Function to analyze the binary search
+    static void analyzeBinarySearchRecursive() {
+        ArrayList<Integer> inputSizes = new ArrayList<>();
+        inputSizes.add(10);
+        inputSizes.add(20);
+        inputSizes.add(30);
+        inputSizes.add(40);
+        inputSizes.add(50);
+        inputSizes.add(100);
 
-    cout<<"=================== Input Size: "<<size<<" ==================="<<endl;
-    stepCount=0;
-    recursiveBinarySearch(arr,0,size-1,bestcase);
-    cout<<"Best Case= "<<stepCount<<" steps"<<endl;
-    stepCount=0;
-    recursiveBinarySearch(arr,0,size-1,randomcase);
-    cout<<"Average Case= "<<stepCount<<" steps"<<endl;
-    stepCount=0;
-    recursiveBinarySearch(arr,0,size-1,worstcase);
-    cout<<"Worst Case= "<<stepCount<<" steps"<<endl;
-    cout<<"==================================================================="<<endl<<endl;
+        for (int size : inputSizes) {
+            ArrayList<Integer> arr = new ArrayList<>(size);
+            for (int i = 0; i < size; i++) {
+                arr.add(i + 1);
+            }
+
+            // Best case: target is the middle element
+            int bestCase = arr.get((size - 1) / 2);
+            // Worst case: target is the last element
+            int worstCase = arr.get(size - 1);
+            // Random case: target is a random element
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(size);
+            int randomCase = arr.get(randomIndex);
+
+            System.out.println("=================== Input Size: " + size + " ===================");
+            
+            // Reset stepCount and perform best case search
+            stepCount = 0;
+            recursiveBinarySearch(arr, 0, size - 1, bestCase);
+            System.out.println("Best Case = " + stepCount + " steps");
+
+            // Reset stepCount and perform average case search
+            stepCount = 0;
+            recursiveBinarySearch(arr, 0, size - 1, randomCase);
+            System.out.println("Average Case = " + stepCount + " steps");
+
+            // Reset stepCount and perform worst case search
+            stepCount = 0;
+            recursiveBinarySearch(arr, 0, size - 1, worstCase);
+            System.out.println("Worst Case = " + stepCount + " steps");
+
+            System.out.println("===================================================================");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        analyzeBinarySearchRecursive();
     }
 }
-
-int main(){
-    analyzbinarysearchRecursive();
-    return 0;
-}
-
-
-
